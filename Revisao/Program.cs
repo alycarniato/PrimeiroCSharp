3cb4774571aa8e0b0851ac048592dedb1d8d00c6
alycarniato <carniato.alysson@gmail.com>
2021-10-26 09:39:53 -0300

    criado o enum de conceito de notas e adicionado na implementacao do menu calcular media

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -59,8 +59,29 @@ namespace Revisao
                         }
 
                         var mediaGeral = notaTotal/nrAlunos;
+                        Conceito conceitoGeral;
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if(mediaGeral < 4 )
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if(mediaGeral < 6 )
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if(mediaGeral < 8 )
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
 
-                        Console.WriteLine($"Média Geral: {mediaGeral}");
+                        Console.WriteLine($"Média Geral: {mediaGeral} - Conceito Geral: {conceitoGeral}");
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
