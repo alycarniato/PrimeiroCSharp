b6a11542d9dfab54adce74a83875e5fd1096de8e
alycarniato <carniato.alysson@gmail.com>
2021-10-26 09:09:14 -0300

    Criado a classe aluno e implementado os menus adicionar e listar

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,23 +6,39 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a opção desejada:");
-            Console.WriteLine("1- Inserir novo aluno");
-            Console.WriteLine("2- Listar alunos");
-            Console.WriteLine("3- Calcular média geral");
-            Console.WriteLine("X- Sair \n");
-
-            string opcaoUsuario = Console.ReadLine();
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
             {
-                switch(opcaoUsuario)
+                switch (opcaoUsuario)
                 {
                     case "1":
-                        //TODO: adicionar alunos
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
                         break;
                     case "2":
-                        //TODO: listar alunos
+                        foreach (var a in alunos)
+                        {
+                            Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                        }
                         break;
                     case "3":
                         //TODO: calcular média geral
@@ -31,8 +47,24 @@ namespace Revisao
                         throw new ArgumentOutOfRangeException();
 
                 }
+
+                opcaoUsuario = ObterOpcaoUsuario();
             }
-            
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair \n");
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
